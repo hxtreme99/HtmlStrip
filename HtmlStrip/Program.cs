ce0b730c7f9f6bb107841d99fe70bb94e0787d0d
cristovaosg <cristovaosg09@gmail.com>
2020-11-14 19:08:04 +0000

    first

--- HtmlStrip/Program.cs
+++ HtmlStrip/Program.cs
@@ -1,6 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Linq;
+﻿using System;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.IO;
@@ -9,18 +7,15 @@ namespace HtmlStrip
 {
     class Program
     {
-       static string split_string(string [] separator,string concatenar)
+        static string split_string(string[] separator, string concatenar)
         {
-            
-            string[] tokens_b = concatenar.Split(separator, StringSplitOptions.None);
+            var tokens_b = concatenar.Split(separator, StringSplitOptions.None);
 
             concatenar = "";
-            for (int i = 0; i < tokens_b.Length; i += 1)
-            {
-                concatenar = string.Concat(concatenar, tokens_b[i]);
-            }
+            for (var i = 0; i < tokens_b.Length; i += 1) concatenar = string.Concat(concatenar, tokens_b[i]);
             return concatenar;
         }
+
         static void Main(string[] args)
         {
             
@@ -31,55 +26,52 @@ namespace HtmlStrip
                 return;
             }
 
+            string code = "";
+            string[] tokens=args[0].Split('.');
 
-
-            if (!File.Exists(args[0]))
+            if (tokens[tokens.Length - 1].Equals("html"))
             {
-                Console.WriteLine($"Error: file {args[0]} does not exist.");
-                return;
-            }
-            string code = System.IO.File.ReadAllText(args[0]);
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine($"Error: file {args[0]} does not exist.");
+                    return;
+                }
+                code = System.IO.File.ReadAllText(args[0]);
 
-            /*
-            WebClient client = new WebClient();
-            string teste1 = "https://pt.wikipedia.org/wiki/Madeira_(regi%C3%A3o_aut%C3%B3noma)";
-            string teste2 = "https://pt.lipsum.com/feed/html";
-            string teste3 = "https://pt.wikipedia.org/wiki/Poncha";
-            string teste4 = "https://pt.wikipedia.org/wiki/Portugal";
-            string downloadString = client.DownloadString(teste4);
-            */
+            }
+
+            else
+            {
+
+                WebClient client = new WebClient();
+                string teste1 = "https://pt.wikipedia.org/wiki/Madeira_(regi%C3%A3o_aut%C3%B3noma)";
+                string teste2 = "https://pt.lipsum.com/feed/html";
+                string teste3 = "https://pt.wikipedia.org/wiki/Poncha";
+                string teste4 = "https://pt.wikipedia.org/wiki/Portugal";
+                code = client.DownloadString(teste4);
+            }
             //string[] tokens_p=Regex.Split(downloadString, @"<[p]>|<[p][ ][^<>]+>|</[p]>");
 
             string[] tokens_p = Regex.Split(code, @"<[p]>|<[p][ ][^<>]+>|</[p]>");
             string concatenar = "";
             for (int i = 1;i< tokens_p.Length;i+=2)
+
             {
-                concatenar=string.Concat(concatenar,tokens_p[i]);
-                concatenar = string.Concat(concatenar,"\n");
+                concatenar = string.Concat(concatenar, tokens_p[i]);
+                concatenar = string.Concat(concatenar, "\n");
             }
 
-            /*
-            string[] tokens_ahref = Regex.Split(concatenar, @"<[a][ ][^<>]+>|</[a]>");
-            concatenar = "";
-            for (int i = 0; i < tokens_ahref.Length; i += 1)
-            {
-                concatenar = string.Concat(concatenar, tokens_ahref[i]);
-            }
 
-            concatenar=split_string(new string[] { "<b>", "</b>" },concatenar);
-            concatenar = split_string(new string[] { "<i>", "</i>" },concatenar);
-
-           */
             
             string[] tokens_apagar = Regex.Split(concatenar, @"<[^<>]+>");
+
             concatenar = "";
-            for (int i = 0; i < tokens_apagar.Length; i += 1)
-            {
+            for (var i = 0; i < tokens_apagar.Length; i += 1) { 
                 concatenar = string.Concat(concatenar, tokens_apagar[i]);
             }
-            
-            Console.WriteLine(concatenar);
-        }
 
+        Console.WriteLine(concatenar);
+
+        }
     }
 }
