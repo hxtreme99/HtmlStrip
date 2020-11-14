2b03157b82024a4c8a70f0ff494066ec8ac89522
Henrique <18406229+hxtreme99@users.noreply.github.com>
2020-11-14 17:23:51 +0000

    first.

--- HtmlStrip/Program.cs
+++ HtmlStrip/Program.cs
@@ -1,6 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Linq;
+﻿using System;
 using System.Net;
 using System.Text.RegularExpressions;
 
@@ -8,58 +6,56 @@ namespace HtmlStrip
 {
     class Program
     {
-       static string split_string(string [] separator,string concatenar)
+        static string split_string(string[] separator, string concatenar)
         {
-            
-            string[] tokens_b = concatenar.Split(separator, StringSplitOptions.None);
+            var tokens_b = concatenar.Split(separator, StringSplitOptions.None);
 
             concatenar = "";
-            for (int i = 0; i < tokens_b.Length; i += 1)
-            {
-                concatenar = string.Concat(concatenar, tokens_b[i]);
-            }
+            for (var i = 0; i < tokens_b.Length; i += 1) concatenar = string.Concat(concatenar, tokens_b[i]);
             return concatenar;
         }
+
         static void Main(string[] args)
         {
+            var client = new WebClient();
+            var teste1 = "https://pt.wikipedia.org/wiki/Madeira_(regi%C3%A3o_aut%C3%B3noma)";
+            var teste2 = "https://pt.lipsum.com/feed/html";
+            var teste3 = "https://pt.wikipedia.org/wiki/Poncha";
+            var teste4 = "https://pt.wikipedia.org/wiki/Portugal";
+            var downloadString = client.DownloadString(teste4);
 
-            WebClient client = new WebClient();
-            string teste1 = "https://pt.wikipedia.org/wiki/Madeira_(regi%C3%A3o_aut%C3%B3noma)";
-            string teste2 = "https://pt.lipsum.com/feed/html";
-            string teste3 = "https://pt.wikipedia.org/wiki/Poncha";
-            string teste4 = "https://pt.wikipedia.org/wiki/Portugal";
-            string downloadString = client.DownloadString(teste4);
+            var tokens_p = Regex.Split(downloadString, @"<[p]>|<[p][ ][^<>]+>|</[p]>");
 
-            string[] tokens_p=Regex.Split(downloadString, @"<[p]>|<[p][ ][^<>]+>|</[p]>");
-            
-            string concatenar = "";
-            for (int i = 1;i< tokens_p.Length;i+=2)
+            var concatenar = "";
+            for (var i = 1; i < tokens_p.Length; i += 2)
             {
-                concatenar=string.Concat(concatenar,tokens_p[i]);
-                concatenar = string.Concat(concatenar,"\n");
+                concatenar = string.Concat(concatenar, tokens_p[i]);
+                concatenar = string.Concat(concatenar, "\n");
             }
 
-            string[] tokens_ahref = Regex.Split(concatenar, @"<[a][ ][^<>]+>|</[a]>");
+            var tokens_ahref = Regex.Split(concatenar, @"<[a][ ][^<>]+>|</[a]>");
             concatenar = "";
-            for (int i = 0; i < tokens_ahref.Length; i += 1)
-            {
+            for (var i = 0; i < tokens_ahref.Length; i += 1)
                 concatenar = string.Concat(concatenar, tokens_ahref[i]);
-            }
 
-            concatenar=split_string(new string[] { "<b>", "</b>" },concatenar);
-            concatenar = split_string(new string[] { "<i>", "</i>" },concatenar);
-
-           
-            
-            string[] tokens_apagar = Regex.Split(concatenar, @"<[^<>]+>");
-            concatenar = "";
-            for (int i = 0; i < tokens_apagar.Length; i += 1)
+            concatenar = split_string(new[]
             {
+                "<b>",
+                "</b>"
+            }, concatenar);
+            concatenar = split_string(new[]
+            {
+                "<i>",
+                "</i>"
+            }, concatenar);
+
+            var tokens_apagar = Regex.Split(concatenar, @"<[^<>]+>");
+            concatenar = "";
+            for (var i = 0; i < tokens_apagar.Length; i += 1)
                 concatenar = string.Concat(concatenar, tokens_apagar[i]);
-            }
 
             Console.WriteLine(concatenar);
+            Console.WriteLine("AHHHHHHHHHHH");
         }
-
     }
 }
